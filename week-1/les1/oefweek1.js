4fca5579cb33d9f236df4b1cec269ae0a8015a02
pgm-kobe <kobe.dekuysscher@student.arteveldehs.be>
2025-11-19 16:16:47 +0100

    making excercises

---
+++ week-1/les1/oefweek1.js
@@ -0,0 +1,12 @@
+function greeting(){
+    return "hallo daar"
+    console.log(greeting)
+}
+
+
+function greeting(anna){
+    const name = "anna"
+    return "hallo " + name
+    console.log(greeting)
+}
+
