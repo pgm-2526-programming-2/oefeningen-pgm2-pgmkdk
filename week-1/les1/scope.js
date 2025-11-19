a70e342064ae3b82993e9bffd38e3986c54592ed
pgm-kobe <kobe.dekuysscher@student.arteveldehs.be>
2025-11-19 15:55:41 +0100

    changes doc

---
+++ week-1/les1/scope.js
@@ -0,0 +1,14 @@
+const message = "hey daar!";
+
+
+function sayHello() {
+    const message = "Bye!";
+
+
+    function say() {
+        const newMessage = "YOLO";
+        console.log(message);
+    }
+
+    console.log(newMessage)
+}
